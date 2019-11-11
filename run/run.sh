9e97272a04093e2dc5b72f125e5e33d0a100737b
cranelv <cranelv@163.com>
2019-11-11 17:05:09 +0800

    modify

--- run/run.sh
+++ run/run.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-./gwallet tuple MAN.4BRmmxsC9iPPDyr8CRpRKUcp7GAww MAN.rp9DVBpuVHwPQGYGeLDHCtRnAA3n 10.1 R7c5Rsrj1Q7r4d5fp
+./gwallet tuple MAN.4BRmmxsC9iPPDyr8CRpRKUcp7GAww MAN.rp9DVBpuVHwPQGYGeLDHCtRnAA3n 10.1 1111
